9031c7d63437e107be42f990ab89c493bb6f29e9
nyne <me@nyne.dev>
2026-01-23 11:42:42 +0000

    fix: vite config

--- frontend_v2/vite.config.ts
+++ frontend_v2/vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  define: {
+    'import.meta.env.CLOUDFLARE_TURNSTILE_SITE_KEY': JSON.stringify(process.env.CLOUDFLARE_TURNSTILE_SITE_KEY),
+  },
   server: {
     allowedHosts: ["debug.nysoure.com"],
     host: "0.0.0.0",
